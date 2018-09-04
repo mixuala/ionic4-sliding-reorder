e310c3cc515cc7015cab458454e4803bac073365
michael lin <mlininkl@gmail.com>
2018-09-04 23:38:42 +0800

    ion-item-sliding stops working for ion-reorder rows that have been moved

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public items:string[] = ['dogs', 'cats', 'birds', 'fishes', 'emus'];
+
+  public remove(item:string){
+    console.log("reomove: ", item)
+  }
+
+  public reorder(ev){
+    const {from, to} = ev.detail;
+    const log = `${this.items[from]} moved to position ${to}: `;
+    this.items.splice(to, 0, this.items.splice(from, 1)[0]);
+    console.log(  log, this.items )
+
+  }
+
+  public delete(o, item) {
+    this.close(o, item)
+    this.items = this.items.filter( v=>v!=item)
+    this.remove(item)
+  }
+
+  public close(o, item){
+    const el = o.currentTarget.closest('ion-item-sliding');
+    el.close()
+    console.log("Close:", item)
+  }
 }

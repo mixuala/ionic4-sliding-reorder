15ac2914683f6cb84c98cb74d1b42433096ef3b6
michael lin <mlininkl@gmail.com>
2018-10-01 16:05:34 +0800

    use ngIf over ngSwitch, set ion-reorder-group[disabled] Notes: ion-sliding works until first delete, then stops. BUT    after ion-sliding stops working, then ion-reorder begins to work, but not before

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  
+
+  ngOnInit() {
+    this.subject.next(this.animals);
+    this.carSubject.next(this.cars);
+    this.activate('1');
+  }
+
+  show:string;
+
+  animals:string[] = ['dog', 'cat', 'fish', 'bird'];
+  subject:BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+  animals$:Observable<string[]> = this.subject;
+
+  cars:any[] = [{label:'audi'}, {label:'bmw'}, {label:'ford'}, {label:'tesla'}];
+  carSubject:BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  cars$:Observable<string[]> = this.carSubject;
+
+  animalsRemove(o:any){
+    let found = this.animals.findIndex(item=>item==o);
+    if (~found) this.animals.splice(found,1);
+    this.subject.next(this.animals);
+  }
+  carsRemove(o:any) {
+    o.hide=true;
+    this.carSubject.next(this.cars.slice());
+  }
+
+  activate(v:string){
+    Object.keys(this.toggle.disabled).forEach( (k:string)=>{this.toggle.disabled[k]=v!=k});
+    this.show = v;
+  }
+
+  toggle:any = {
+    disabled:{
+      '1':false,
+      '2':true,
+      '3':true,
+    },
+  }
+
+
+
   public items:string[] = ['dogs', 'cats', 'birds', 'fishes', 'emus'];
 
   public remove(item:string){
-    console.log("reomove: ", item)
+    console.log("remove: ", item)
   }
 
   public reorder(ev){
